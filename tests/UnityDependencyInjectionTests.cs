3c22e149d613c017f168ebdd54dbfa19a03f6f84
Eugene Sadovoi <evgeni@eniks.com>
2017-11-19 20:32:49 -0500

    Added new Specification Tests

--- tests/UnityDependencyInjectionTests.cs
+++ tests/UnityDependencyInjectionTests.cs
@@ -5,22 +5,22 @@ using Microsoft.Extensions.DependencyInjection.Specification;
 
 namespace Unity.Microsoft.DependencyInjection.Tests
 {
-  public sealed class UnityDependencyInjectionTests : DependencyInjectionSpecificationTests
-  {
-    /// <inheritdoc />
-    protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
-    {
-      var container = new UnityContainer();
-      var provider = new ServiceProvider(container);
+    //public sealed class UnityDependencyInjectionTests : DependencyInjectionSpecificationTests
+    //{
+    //    /// <inheritdoc />
+    //    protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+    //    {
+    //        var container = new UnityContainer();
+    //        var provider = new ServiceProvider(container);
 
-      foreach (ServiceDescriptor service in serviceCollection)
-      {
-        container.RegisterType(
-          service.ServiceType,
-          service.ImplementationType);
-      }
+    //        foreach (ServiceDescriptor service in serviceCollection)
+    //        {
+    //            container.RegisterType(
+    //              service.ServiceType,
+    //              service.ImplementationType);
+    //        }
 
-      return provider;
-    }
-  }
+    //        return provider;
+    //    }
+    //}
 }
