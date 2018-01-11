c1ef821b112d6f85af90ff0301e3fdf8995a1868
Eugene Sadovoi <evgeni@eniks.com>
2018-01-10 19:25:33 -0500

    Releasing v2.0.2

--- tests/UnityDependencyInjectionTests.cs
+++ tests/UnityDependencyInjectionTests.cs
@@ -15,41 +15,5 @@ namespace Unity.Microsoft.DependencyInjection.Tests
         {
             return ServiceProvider.ConfigureServices(serviceCollection);
         }
-
-
-        [Fact]
-#pragma warning disable xUnit1024 // Test methods cannot have overloads
-        public new void DisposesInReverseOrderOfCreation()
-#pragma warning restore xUnit1024 // Test methods cannot have overloads
-        {
-
-            // Arrange
-            var serviceCollection = new TestServiceCollection();
-            serviceCollection.AddSingleton<FakeDisposeCallback>();
-            serviceCollection.AddTransient<IFakeOuterService, FakeDisposableCallbackOuterService>();
-            serviceCollection.AddSingleton<IFakeMultipleService, FakeDisposableCallbackInnerService>();
-            serviceCollection.AddScoped<IFakeMultipleService, FakeDisposableCallbackInnerService>();
-            serviceCollection.AddTransient<IFakeMultipleService, FakeDisposableCallbackInnerService>();
-            serviceCollection.AddSingleton<IFakeService, FakeDisposableCallbackInnerService>();
-            var serviceProvider = CreateServiceProvider(serviceCollection);
-
-            var callback = serviceProvider.GetService<FakeDisposeCallback>();
-            var outer = serviceProvider.GetService<IFakeOuterService>();
-            var multipleServices = outer.MultipleServices.ToArray();
-
-            // Act
-            ((IDisposable)serviceProvider).Dispose();
-
-            // Assert
-            //Assert.Equal(outer, callback.Disposed[0]);
-            //Assert.Equal(outer.MultipleServices.Reverse(), callback.Disposed.Skip(1).Take(3).OfType<IFakeMultipleService>());
-            //Assert.Equal(outer.SingleService, callback.Disposed[4]);
-
-        }
-
-
-        internal class TestServiceCollection : List<ServiceDescriptor>, IServiceCollection, IList<ServiceDescriptor>, ICollection<ServiceDescriptor>, IEnumerable<ServiceDescriptor>, IEnumerable
-        {
-        }
     }
 }
