62baf7fa13c882e5b3aa097ec7aa6cfb12c07fac
Eugene Sadovoi <evgeni@eniks.com>
2019-12-08 12:22:49 -0800

    Release/5.11 (#65)
    * Upgrading to 3.x
    
    * Releasing 5.11.2

--- src/Extensions/HostingExtension.cs
+++ src/Extensions/HostingExtension.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 
 namespace Unity.Microsoft.DependencyInjection
 {
     public static class HostingExtension
     {
         private static ServiceProviderFactory _factory;
+
+
+        public static IHostBuilder UseUnityServiceProvider(this IHostBuilder hostBuilder, IUnityContainer container = null)
+        {
+            _factory = new ServiceProviderFactory(container);
+
+            return hostBuilder.ConfigureServices((context, services) =>
+            {
+                services.Replace(ServiceDescriptor.Singleton<IServiceProviderFactory<IUnityContainer>>(_factory));
+                services.Replace(ServiceDescriptor.Singleton<IServiceProviderFactory<IServiceCollection>>(_factory));
+            });
+        }
 
         public static IWebHostBuilder UseUnityServiceProvider(this IWebHostBuilder hostBuilder, IUnityContainer container = null)
         {
