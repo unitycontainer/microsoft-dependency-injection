39f28bca27aac82262e2eaa75f0f32c825e9bdc6
Thomas Ibel <tibel@users.noreply.github.com>
2020-07-07 17:09:45 +0200

    use new hosting abstractions

--- src/Extensions/HostingExtension.cs
+++ src/Extensions/HostingExtension.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 
@@ -20,25 +19,6 @@ namespace Unity.Microsoft.DependencyInjection
                                   services.Replace(ServiceDescriptor.Singleton<IServiceProviderFactory<IUnityContainer>>(_factory));
                                   services.Replace(ServiceDescriptor.Singleton<IServiceProviderFactory<IServiceCollection>>(_factory));
                               });
-        }
-
-        public static IWebHostBuilder UseUnityServiceProvider(this IWebHostBuilder hostBuilder, IUnityContainer container = null)
-        {
-            _factory = new ServiceProviderFactory(container);
-
-#if NETCOREAPP1_1
-            return hostBuilder.ConfigureServices((services) =>
-            {
-                services.Replace(ServiceDescriptor.Singleton<IServiceProviderFactory<IUnityContainer>>(_factory));
-                services.Replace(ServiceDescriptor.Singleton<IServiceProviderFactory<IServiceCollection>>(_factory));
-            });
-#else
-            return hostBuilder.ConfigureServices((context, services) =>
-            {
-                services.Replace(ServiceDescriptor.Singleton<IServiceProviderFactory<IUnityContainer>>(_factory));
-                services.Replace(ServiceDescriptor.Singleton<IServiceProviderFactory<IServiceCollection>>(_factory));
-            });
-#endif
         }
     }
 }
